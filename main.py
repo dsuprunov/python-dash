e0e217e352c7fd315b6c0babc80acb5bae9c4096
dsuprunov <dsuprunov@gmail.com>
2023-11-22 14:35:23 +0100

    add graph

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from dash import Dash
 from dash import html
 from dash import dash_table
+from dash import dcc
+from dash import callback
+from dash import Output
+from dash import Input
 import pandas as pd
-
+import plotly.express as px
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
 app = Dash(__name__)
 app.layout = html.Div([
-    html.Div(children='My First App with Data'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+    html.Div(children='gapminder2007.csv'),
+    html.Hr(),
+    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+    dcc.Graph(figure={}, id='controls-and-graph')
 ])
+
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
 
 
 if __name__ == '__main__':
