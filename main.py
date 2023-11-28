53c7013c5736c7ac3d07452886e938d00b3aa5c4
Dmitry Suprunov <dsuprunov@gmail.com>
2023-11-28 19:24:51 +0100

    readme and tasks definitions

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
 from dash import Dash
 from dash import html
 from dash import dash_table
-from dash import dcc
-from dash import callback
-from dash import Output
-from dash import Input
 import pandas as pd
-import plotly.express as px
+import numpy as np
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+import config
+
+
+df = pd.read_csv(config.DATA_CSV)
+df.replace('?', np.nan, inplace=True)
 
 app = Dash(__name__)
 app.layout = html.Div([
-    html.Div(children='gapminder2007.csv'),
-    html.Hr(),
-    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='controls-and-graph')
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
 ])
-
-@callback(
-    Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
 
 
 if __name__ == '__main__':
