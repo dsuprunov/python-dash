d75da35dcd8ae36a29c3040f06a74b2d88aedd4e
Dmitry Suprunov <dsuprunov@gmail.com>
2023-11-30 23:20:22 +0100

    new layout

--- app.py
+++ app.py
@@ -2,104 +2,79 @@ from dash import Dash, dcc, html, Input, Output, callback, dash_table
 import plotly.express as px
 import pandas as pd
 import numpy as np
+from icecream import ic
 
 import config
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = Dash(
     __name__,
-    external_stylesheets=external_stylesheets,
-    suppress_callback_exceptions=True
+    suppress_callback_exceptions=True,
 )
 
 app.layout = html.Div([
-    dcc.Tabs(id='tabs-labels', value='tab-explore', children=[
+    dcc.Tabs(id='tabs-main-labels', value='tab-explore', children=[
         dcc.Tab(label='Explore', value='tab-explore'),
         dcc.Tab(label='Visualize', value='tab-visualize'),
         dcc.Tab(label='Export', value='tab-export'),
     ]),
-    html.Div(id='tabs-content')
+    html.Div(id='tabs-main-content')
 ])
 
 
-@callback(Output('tabs-content', 'children'),
-          Input('tabs-labels', 'value'))
-def render_tab_content(tab):
+@callback(Output('tabs-main-content', 'children'),
+          Input('tabs-main-labels', 'value'))
+def render_tabs_main_content(tab):
+    df = pd.read_csv(config.DATA_CSV)
+    df.replace('?', np.nan, inplace=True)
+
     match tab:
         case 'tab-explore':
-            df = pd.read_csv(config.DATA_CSV)
-            df.replace('?', np.nan, inplace=True)
-
             return html.Div([
-                html.Br(),
+                dcc.Dropdown(
+                    df.columns,
+                    id='tab-explore-data-filter',
+                    value=df.columns,
+                    multi=True,
+                ),
                 dash_table.DataTable(
-                    id='table',
+                    id='tab-explore-data-table',
                     columns=[{'name': i, 'id': i} for i in df.columns],
                     data=df.to_dict('records'),
                     page_size=10,
                     page_action='native',
                 ),
                 dcc.Dropdown(
-                    id='tab-explore-columns-dropdown',
+                    id='tab-explore-inspect-filter',
                     options=[{'label': col, 'value': col} for col in df.columns],
                     value=''
                 ),
-                html.Div(id='tab-explore-columns-output')
+                html.Div(id='tab-explore-inspect-graph'),
             ])
 
         case 'tab-visualize':
             return html.Div([
-                dcc.Graph(
-                    id='tab-visualize-graph',
-                    figure={
-                        'data': [{
-                            'x': [1, 2, 3],
-                            'y': [5, 10, 6],
-                            'type': 'bar'
-                        }]
-                    }
-                )
+                html.Div('Visualize: Nothing here yet...')
             ])
 
         case 'tab-export':
             return html.Div([
-                html.Div('Nothing here yet...')
+                html.Div('Export: Nothing here yet...')
             ])
 
         case _:
             html.Div('Oops, something went wrong. You should not see this.')
 
 
-@app.callback(
-    Output('tab-explore-columns-output', 'children'),
-    [Input('tab-explore-columns-dropdown', 'value')]
-)
-def render_explore_content(selected_column):
-    if selected_column == '':
-        return ''
+@callback([Output('tab-explore-data-table', 'columns'),
+           Output('tab-explore-inspect-filter', 'options'),
+           Output('tab-explore-inspect-graph', 'children')],
+          Input('tab-explore-data-filter', 'value'))
+def render_tab_explore_data_table(columns):
+    if not columns:
+        return [], [], html.Div()
 
-    df = pd.read_csv(config.DATA_CSV)
-    df.replace('?', np.nan, inplace=True)
-
-    if selected_column not in df.columns:
-        return ''
-
-    df = df[selected_column].value_counts(normalize=True) * 100
-    df = df.reset_index()
-
-    return dcc.Graph(
-        id='tab-explore-columns-output-graph',
-        figure=px.bar(
-            df,
-            x=selected_column,
-            y='proportion',
-            labels={'selected_column': 'Percentage (%)'},
-            title='Percentage Distribution'
-        ),
-    )
-
+    return [{'name': col, 'id': col} for col in columns], columns, html.Div()
 
 
 if __name__ == '__main__':
